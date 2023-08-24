b1e9f5b86e8028e441421a934426d49a926d25e5
Zahid <working05523@gmail.com>
2023-08-24 10:22:56 -0700

    done

---
+++ app.js
@@ -0,0 +1,64 @@
+// FUNCTION - DEFINED FUNCTION
+
+//========================1
+
+// function abc(){
+//     console.log("Hello World");
+// }
+// abc();
+
+//=======================2
+
+// parameter and argument
+// DYNAMIC FUNCTION
+
+// function abc(a){ // a is parameter
+//     console.log('HellO '+a);
+// }
+// abc("Zahid"); // argument
+
+//======================3
+
+// function abc(firstName, lastName){
+//     var fullName =(firstName+" "+lastName);
+//     return fullName;
+// }
+// var  a=abc("Zahid","Khan");
+// console.log(a);
+
+
+
+// CALCULATOR
+// function abc(num1,num2,operator){
+//     if(operator==="+"){
+//     var calCulated=(num1+num2);
+//     return calCulated;
+//     }
+//     else if(operator==="-"){
+//     var calCulated=(num1-num2);
+//     return calCulated;
+//     }
+//     else if(operator==="*"){
+//     var calCulated=(num1*num2);
+//     return calCulated;
+//     }
+//     else if(operator==="/"){
+//     var calCulated=(num1/num2);
+//     return calCulated;
+//     }
+// }
+// var a=abc(20,10,"/");
+// console.log(a);
+
+//=============================4
+
+//LOCAL & GLOBAL VARIABLE
+
+//GLOBAL VARIABLE ACCESS COMPLETE FILE
+
+// var a='zahid'; // global
+//  function abc (){
+//     var b='zahid'; // local variable
+//     console.log(a);
+//  }
+//  abc();
